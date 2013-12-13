595db77f15dcf87ac62a304043208fd5534985e3
Ben Gordon <brgordon@ua.edu>
2013-12-13 12:20:51 -0600

    Initial Commit

---
+++ IMQuickSearch/IMViewController.h
@@ -0,0 +1,13 @@
+//
+//  IMViewController.h
+//  IMQuickSearch
+//
+//  Created by Ben Gordon on 12/13/13.
+//  Copyright (c) 2013 Intermark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IMViewController : UIViewController
+
+@end
---
+++ IMQuickSearch/IMViewController.m
@@ -0,0 +1,29 @@
+//
+//  IMViewController.m
+//  IMQuickSearch
+//
+//  Created by Ben Gordon on 12/13/13.
+//  Copyright (c) 2013 Intermark. All rights reserved.
+//
+
+#import "IMViewController.h"
+
+@interface IMViewController ()
+
+@end
+
+@implementation IMViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
