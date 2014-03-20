300439fddc74c3f8096a8e9d59e141f918b83e33
Ben Gordon <brgordon@ua.edu>
2014-03-20 10:57:28 -0500

    Changes
    - Added License
    - Added array filtering

--- Classes/IMQuickSearchFilter.m
+++ Classes/IMQuickSearchFilter.m
@@ -1,10 +1,23 @@
+//  The MIT License (MIT)
 //
-//  IMQuickSearchFilter.m
-//  IMQuickSearch
+//  Copyright (c) 2014 Intermark Interactive
 //
-//  Created by Ben Gordon on 12/13/13.
-//  Copyright (c) 2013 Intermark. All rights reserved.
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of
+//  this software and associated documentation files (the "Software"), to deal in
+//  the Software without restriction, including without limitation the rights to
+//  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+//  the Software, and to permit persons to whom the Software is furnished to do so,
+//  subject to the following conditions:
 //
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+//  FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+//  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+//  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #import "IMQuickSearchFilter.h"
 
@@ -63,6 +76,19 @@
     
     // An object must have a property for the key
     if (![obj valueForKey:key]) {
+        return NO;
+    }
+    
+    // If it's an NSArray, loop through
+    if ([[obj valueForKey:key] isKindOfClass:[NSArray class]]) {
+        for (id arrObject in [obj valueForKey:key]) {
+            if ([arrObject isKindOfClass:[NSString class]] || [value isKindOfClass:[NSString class]]) {
+                if ([self checkString:arrObject withString:value]) {
+                    return YES;
+                }
+            }
+        }
+        
         return NO;
     }
     
